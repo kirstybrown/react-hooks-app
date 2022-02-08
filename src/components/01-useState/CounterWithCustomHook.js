e58b7cd9b7461c79ebeb13c96b7966880dbea88d
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-02-08 18:05:01 +0100

    added components and custom hooks for useState and useEffect

---
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useCounter } from '../../hooks/useCounter';
+
+import './counter.css';
+
+const CounterWithCustomHook = () => {
+
+    const { state, increment, reset, decrement } = useCounter();
+
+  return (
+      <>
+        <h1>Counter with Hook: { state } </h1>
+        <hr />
+
+        <button onClick={ () => increment(2) } className='btn'> + 1 </button>
+        <button onClick={ reset } className='btn'> reset </button>
+        <button onClick={ () => decrement(2) } className='btn'> - 1 </button>
+      </>
+  )
+};
+
+export default CounterWithCustomHook;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HookApp from './HookApp';
+// import CounterApp from './components/01-useState/CounterApp';
+// import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
+import SimpleForm from './components/components/02-useEffect/SimpleForm';
+
 
 
 ReactDOM.render(
 
-    <HookApp />,
+    <SimpleForm />,
 
     document.getElementById('root')
 );
