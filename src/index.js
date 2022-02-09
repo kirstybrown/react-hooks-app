9478c74f2f10e16c72b68850dfbafb01fbc1514b
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-02-09 13:19:30 +0100

    updated MultipleCustomHooks component and project structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FormWithCustomHook from './components/components/02-useEffect/FormWithCustomHooks';
+import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
+// import FormWithCustomHook from './components/components/02-useEffect/FormWithCustomHooks';
 // import CounterApp from './components/01-useState/CounterApp';
 // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 // import SimpleForm from './components/components/02-useEffect/SimpleForm';
@@ -9,7 +10,7 @@ import FormWithCustomHook from './components/components/02-useEffect/FormWithCus
 
 ReactDOM.render(
 
-    <FormWithCustomHook />,
+    <MultipleCustomHooks />,
 
     document.getElementById('root')
 );
