62141367fde3a64969bf4ba37e813ea9cf679014
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-02-10 23:31:07 +0100

    added UseForm tests

---
+++ src/tests/hooks/UseForm.test.js
@@ -0,0 +1,19 @@
+
+
+describe('Tests for useForm', () => { 
+
+    const initialForm = {
+        name: 'kirsty',
+        email: 'kirsty.a.d.brown@gmail.com'
+    }
+
+    test('should return a default form', () => { 
+
+     });
+
+     test('should change the value on the form (change name)', () => { 
+
+      });
+
+
+ })
