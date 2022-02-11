a51e186e0b9b7cf6275143a174882e55d277400a
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-02-11 18:11:30 +0100

    completed useFetch tests

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -28,7 +28,14 @@ const useFetch = ( url ) => {
                             data
                         })
                     }
-            });
+            })
+            .catch( () => {
+                setState({
+                    data: null,
+                    loading: false,
+                    error: 'No se pudo cargar la info'
+                })
+            })
     }, [url]);
 
     return state;
---
+++ src/tests/hooks/UseFetch.test.js
@@ -0,0 +1,40 @@
+import { renderHook } from "@testing-library/react-hooks";
+import useFetch from '../../hooks/useFetch';
+
+
+describe('Tests in useFetch', () => { 
+    
+    test('should return default info', () => { 
+        
+        const { result } = renderHook( () => useFetch( 'https://www.breakingbadapi.com/api/quotes/${ counter }' ));
+
+        const { data, loading, error } = result.current;
+        expect( data ).toBe(null);
+        expect( loading ).toBe(true);
+        expect( error ).toBe(null);
+     });
+
+     test('should have the desired info, loading false, error false', async() => { 
+        
+        const { result, waitForNextUpdate } = renderHook( () => useFetch( 'https://www.breakingbadapi.com/api/quotes/1' ));
+        await waitForNextUpdate({timeout:5000});
+
+        const { data, loading, error } = result.current;
+
+        expect( data.length ).toBe(1);
+        expect( loading ).toBe(false);
+        expect( error ).toBe( null );
+     });
+
+     test('should control the error', async() => { 
+        
+        const { result, waitForNextUpdate } = renderHook( () => useFetch( 'https://reqres.in/apid/users?page=2' ));
+        await waitForNextUpdate({timeout:5000});
+
+        const { data, loading, error } = result.current;
+
+        expect( data ).toBe(null);
+        expect( loading ).toBe(false);
+        expect( error ).toBe( 'No se pudo cargar la info' );
+     });
+ })
