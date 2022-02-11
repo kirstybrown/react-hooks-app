e8cb5364dc73a6cca42ac53aec2f0073e6ae3d6c
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-02-11 21:02:49 +0100

    added tests for TodoList

---
+++ src/tests/components/08-useReducer/TodoList.test.js
@@ -0,0 +1,29 @@
+import { shallow } from 'enzyme';
+import TodoList from '../../../components/08-useReducer/TodoList';
+import { demoTodos } from '../../fixtures/demoTodos';
+
+describe('Tests for TodoList', () => { 
+    
+    const handleDelete = jest.fn();
+    const handleToggle = jest.fn();
+
+    const wrapper = shallow(
+        <TodoList
+            todos={ demoTodos }
+            handleDelete={ handleDelete }
+            handleToggle={ handleToggle }
+        />
+    )
+
+    test('should show component correctly', () => { 
+
+        expect( wrapper ).toMatchSnapshot();
+     })
+
+     test('should show 2 <TodoListItem />', () => { 
+
+        expect( wrapper.find('TodoListItem').length ).toBe( demoTodos.length );
+
+        expect( wrapper.find('TodoListItem').at(0).prop('handleDelete') ).toEqual( expect.any(Function) );
+     })
+ })
