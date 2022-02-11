2419b6046725752c2a07662b66724eb555b90627
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-02-11 11:38:53 +0100

    completed useForm Custom Hook tests

--- src/tests/hooks/UseForm.test.js
+++ src/tests/hooks/UseForm.test.js
@@ -1,3 +1,5 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useForm from '../../hooks/useForm';
 
 
 describe('Tests for useForm', () => { 
@@ -8,12 +10,55 @@ describe('Tests for useForm', () => {
     }
 
     test('should return a default form', () => { 
+        const { result } = renderHook( () => useForm(initialForm) );
+        const [ formValues, handleInputChange, reset ] = result.current;
+
+        expect( result.current[0].name ).toBe( 'kirsty' );
+        expect( result.current[0].email ).toBe( 'kirsty.a.d.brown@gmail.com' );
+
+        expect( formValues ).toEqual( initialForm );
+        expect( typeof handleInputChange ).toBe( 'function' );
+        expect( typeof reset ).toBe( 'function' );
 
      });
 
-     test('should change the value on the form (change name)', () => { 
+    test('should change the value on the form (change name)', () => { 
+        const { result } = renderHook( () => useForm(initialForm) );
+        const [ , handleInputChange ] = result.current;
 
-      });
+        act( () => {
+            handleInputChange({
+                target: {
+                    name: 'name',
+                    value: 'fernando'
+                }
+            });
+        });
 
+        const [ formValues ] = result.current;
+        console.log(formValues);
+        expect( formValues ).toEqual( { ...initialForm, name: 'fernando' });
+
+    });
+
+    test('should re-establish the form with RESET ', () => { 
+
+        const { result } = renderHook( () => useForm(initialForm) );
+        const [ , handleInputChange, reset ] = result.current;
+
+        act( () => {
+            handleInputChange({
+                target: {
+                    name: 'name',
+                    value: 'fernando'
+                }
+            });
+            reset();
+        });
+
+        const [ formValues ] = result.current;
+        console.log(formValues);
+        expect( formValues ).toEqual( initialForm );
+     })
 
  })
